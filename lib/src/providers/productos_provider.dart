896c28b5d354c6d8cabd5e35f336a2fb8aa8c20b
cesarcunyarache <cesarcunyarache@gmail.com>
2024-06-03 00:25:04 +0000

    correcion metodo crearProducto

--- lib/src/providers/productos_provider.dart
+++ lib/src/providers/productos_provider.dart
@@ -2,15 +2,17 @@ import 'package:myapp/src/models/productos_models.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 class ProductosProvider{
-  final String _url='https://flutter-productos-d730a.firebaseio.com';
+  final String _url='https://flutter-productos-d730a-default-rtdb.firebaseio.com';
   
   Future<bool> crearProducto(ProductoModel producto) async{
     
     final url= '$_url/productos.json';
     
-    final resp = await http.post(url as Uri, body: productoModelToJson(producto));
+    final resp = await http.post( Uri.parse(url),
+    body: jsonEncode(producto.toJson())
+    );
     
-    final decodeData = json.decode(resp.body);
+    final decodeData = json.decode(resp.body); 
     
     print(decodeData);
     
